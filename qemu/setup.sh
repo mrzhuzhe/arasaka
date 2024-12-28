219ea987f856e74b50a4e7e2230e1d11e08fb806
mrzhuzhe <466648706@qq.com>
2024-12-29 01:08:47 +0800

    qemu

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -2,7 +2,6 @@ qemu-img create -f raw my_img 16G
 
 qemu-system-x86_64 \
     -display vnc=127.0.0.1:0 \
-    -vga std \
     -m 4096 \
     -enable-kvm \
     -cdrom archlinux-2024.12.01-x86_64.iso \
