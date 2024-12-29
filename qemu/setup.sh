4104bfe76197ff68800cd259b5814a61f8734c78
mrzhuzhe <466648706@qq.com>
2024-12-29 18:12:11 +0800

    maybe bridge

--- qemu/setup.sh
+++ qemu/setup.sh
@@ -1,9 +1,21 @@
 qemu-img create -f raw my_img 16G
 
+printf -v macaddr "52:54:%02x:%02x:%02x:%02x" $(( $RANDOM & 0xff)) $(( $RANDOM & 0xff )) $(( $RANDOM & 0xff)) $(( $RANDOM & 0xff ))
 qemu-system-x86_64 \
+    -net nic,macaddr="$macaddr" \
     -display vnc=127.0.0.1:0 \
     -m 4096 \
     -enable-kvm \
     -cdrom archlinux-2024.12.01-x86_64.iso \
     -boot order=d \
-    -drive file=my_img,format=raw 
+    -drive file=my_img,format=raw
+
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+mkfs.fat -F 32 /dev/sda1
+
+mount /dev/sda3 /mnt
+mount --mkdir /dev/sda1 /mnt/boot
+swapon /dev/sda2
+
+pacstrap -K /mnt base linux linux-firmware
