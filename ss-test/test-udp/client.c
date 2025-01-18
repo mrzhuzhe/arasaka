54bc362f95e0bfc29901257766d8598864fcd329
mrzhuzhe <466648706@qq.com>
2025-01-18 17:34:26 +0800

    tcp

---
+++ ss-test/test-udp/client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+    
+    struct sockaddr_in svaddr;
+    int sfd = 0, j;
+    size_t msgLen;
+    ssize_t numBytes;
+    char resp[100];
+
+    //memset(&svaddr, 0 , sizeof(svaddr));
+    // AF_UNIX is on same host 
+    sfd = socket(AF_INET, SOCK_DGRAM, 0);
+    svaddr.sin_family = AF_INET;
+    svaddr.sin_addr.s_addr  = INADDR_ANY;
+    svaddr.sin_port = htons(50001);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &svaddr.sin_addr) <= 0){
+        printf("inet_pton fail \n");
+    }
+    char * msg = "test msg";
+    msgLen = strlen(msg);
+    if (sendto(sfd, msg, msgLen, 0, (struct sockaddr *)&svaddr, sizeof(struct sockaddr_in)) != msgLen){
+        printf("sendto fail \n");
+    }
+
+    if (recvfrom(sfd, resp, 100, 0, NULL, NULL) == -1) {
+         printf("recvfrom fail \n");
+    }
+    
+    printf("Response %d: %s\n", 123, resp);
+
+    return 0;
+}
---
+++ ss-test/test-udp/server.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "utils.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+    printf("test abc \n");
+    test_fn();
+    struct sockaddr_in svaddr, claddr;
+    int sfd = 0, j;
+    socklen_t len;
+    char buf[100];
+    char claddrStr[INET_ADDRSTRLEN];
+    ssize_t numBytes;
+
+    sfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sfd == -1) printf("sv socket fail\n"); 
+
+    //memset(&svaddr, 0 , sizeof(svaddr));
+    svaddr.sin_family = AF_INET;
+    svaddr.sin_addr.s_addr  = htonl(INADDR_ANY);
+    svaddr.sin_port = htons(50001);    
+
+    if (bind(sfd, (struct sockaddr *)&svaddr, sizeof(svaddr)) ==-1 ) printf("sv bind fail\n");
+
+    for (;;) {
+        len = sizeof(struct sockaddr_in);
+        numBytes = recvfrom(sfd, buf, 100, 0, (struct sockaddr *)&claddr, &len);
+        if (numBytes == -1) {
+            printf("sv recvfrom fail\n");
+        }
+        if (inet_ntop(AF_INET, &claddr.sin_addr, claddrStr, INET_ADDRSTRLEN) == NULL){
+            printf("Could not convert client address to string \n");
+        } else {
+            printf("Server received %ld bytes from %s %u \n", (long)numBytes, claddrStr, ntohs(claddr.sin_port));
+        }
+        
+        // bussiness  
+        for (j=0;j<numBytes;j++){
+            buf[j] = toupper((unsigned char)buf[j]);           
+        }
+        // bussiness  
+
+        if (sendto(sfd, buf, numBytes, 0, (struct sockaddr *)&claddr, len) != numBytes) {
+             printf("sv sendto fail\n");
+        }
+    }
+
+    return 0;
+}
