aadf2980a1ef23c3de67bc993c24c128c47c42ca
mrzhuzhe <466648706@qq.com>
2025-01-31 18:45:56 +0800

    vfio

--- qemu/0.3/gpu.sh
+++ qemu/0.3/gpu.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
-shopt -s nullglob
-for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
-    echo "IOMMU Group ${g##*/}:"
-    for d in $g/devices/*; do
-        echo -e "\t$(lspci -nns ${d##*/})"
-    done;
-done;
+#  https://documentation.ubuntu.com/server/how-to/graphics/gpu-virtualization-with-qemu-kvm/
+#  vfio https://docs.kernel.org/driver-api/vfio.html
+
+find /sys/kernel/iommu_groups/ -name "*$(lspci | awk '/ 3D / {print $1}')*"
+
+ll /sys/kernel/iommu_groups/2/devices/
+
+echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf          
+echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
+sudo update-initramfs -u                                                         
+sudo reboot                                                                      
+
+lspci -v | grep -A 10 " 3D "
