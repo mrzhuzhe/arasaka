8dbb1955211b86a2a3b82a52a5a6ea74fc456770
mrzhuzhe <466648706@qq.com>
2025-02-06 22:25:54 +0800

    loongson

---
+++ qemu/loongson-test/0.1/run.sh
@@ -0,0 +1,4 @@
+# qemu https://www.qemu.org/docs/master/system/loongarch/virt.html
+# loognson build tools https://github.com/loongson/build-tools 
+# edk2 https://github.com/tianocore/edk2-platforms/blob/master/Platform/Loongson/LoongArchQemuPkg/Readme.md
+# kernel https://github.com/loongson/linux
