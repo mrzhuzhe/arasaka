f0896b05143313a546fd54994b9d80f5010fa84e
mrzhuzhe <466648706@qq.com>
2025-03-23 22:10:12 +0800

    nographic wrong

--- UEFI/loongson.sh
+++ UEFI/loongson.sh
@@ -1,10 +1,4 @@
-# qemu-system-loongarch64 \
-# -m 4G \
-# -M virt \
-# -smp 2 \
-# -cpu la464 \
-# -bios outputs/loongson/QEMU_EFI.fd \
-# -serial stdio
+# Issue -nographic 
 
 
 printf -v macaddr "52:54:%02x:%02x:%02x:%02x" $(( $RANDOM & 0xff)) $(( $RANDOM & 0xff )) $(( $RANDOM & 0xff)) $(( $RANDOM & 0xff ))
@@ -19,9 +13,8 @@ qemu-system-loongarch64 \
     -device virtio-gpu-pci \
     -net nic,macaddr="$macaddr" \
     -net user \
-    -display vnc=127.0.0.1:0 \
+    -display vnc=127.0.0.1:1 \
     -device nec-usb-xhci,id=xhci,addr=0x1b \
     -device usb-tablet,id=tablet,bus=xhci.0,port=1 \
     -device usb-kbd,id=keyboard,bus=xhci.0,port=2 \
-    -hda outputs/archlinux-minimal-2024.04.23-loong64.qcow2 \
-    -nographic
+    -hda outputs/archlinux-minimal-2024.04.23-loong64.qcow2 
