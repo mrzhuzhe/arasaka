76231820b4ffb7dafb6e233259530c785ecdb99c
mrzhuzhe <466648706@qq.com>
2025-07-31 23:37:02 +0800

    travelsal bb

--- reverse/gcc-pass/case/main.c
+++ reverse/gcc-pass/case/main.c
@@ -1,10 +1,32 @@
 #include "stdio.h"
 
+
+// Note:
+// We need 'no_instrument_function' attribute on __cyg_profile_fun_*
+extern "C" {
+    void __inst_profile()
+    {
+        printf("Do something here!\n");
+    }
+
+    // called every time a function get called
+    __attribute__ ((no_instrument_function)) void __cyg_profile_func_enter (void *this_fn, void *call_site)
+    {
+        printf("Entering in 0x%lx from 0x%lx...\n", (unsigned long)this_fn, (unsigned long)call_site);
+    }
+
+    // called every time a function returns
+    __attribute__ ((no_instrument_function)) void __cyg_profile_func_exit  (void *this_fn, void *call_site)
+    {
+        printf("Exiting from 0x%lx to 0x%lx...\n", (unsigned long)this_fn, (unsigned long)call_site);
+    }
+}
+
 void inner(float* res, unsigned i, unsigned j, unsigned w){
     res[i*w+j] = (float)(i*w+j) * (float)(i*w+j);
 }
 
-void loop(float* res, unsigned n, unsigned w){
+__attribute((profiled))  void loop(float* res, unsigned n, unsigned w){
     for(int i=0;i<n;i++){
         for(int j=0;j<w;j++){
             inner(res, i, j, w);
---
+++ reverse/gcc-pass/plugin/showdot.sh
@@ -0,0 +1,2 @@
+dot -Tpdf test.dot > test.pdf
+xdg-open test.pdf
