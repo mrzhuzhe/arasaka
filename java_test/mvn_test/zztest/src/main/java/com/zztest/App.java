589311a2707e7f89495deaba740a9f6bd8f92a0a
mrzhuzhe <466648706@qq.com>
2026-05-27 10:31:25 +0000

    maven shade plugin

---
+++ java_test/mvn_test/zztest/src/main/java/com/zztest/App.java
@@ -0,0 +1,13 @@
+package com.zztest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "zz test Hello World!" );
+    }
+}
